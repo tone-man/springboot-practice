9d81f64addf0771b8219ecb60e628cd1da7df781
tones <thetoneman@outlook.com>
2024-12-28 12:55:40 -0500

    feat: add homepage

--- simple-application/src/main/java/learn/necasdev/simple_application/SimpleApplication.java
+++ simple-application/src/main/java/learn/necasdev/simple_application/SimpleApplication.java
@@ -1,7 +1,12 @@
 package learn.necasdev.simple_application;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 public class SimpleApplication {
@@ -9,5 +14,19 @@ public class SimpleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
 	}
+
+    @Controller
+    private class SimpleController {
+
+        @Value("${spring.application.name}")
+        String appName;
+
+        @GetMapping("/")
+        public String getHomePage(Model model) {
+            model.addAttribute("appName", appName);
+
+            return "home";
+        }
+    }
 
 }
